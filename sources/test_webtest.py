d36f872145ac386717d2d656104e478c8fa2c369
ian <ianfergu@live.unc.edu>
2019-08-05 09:55:59 -0400

    new

--- sources/test_webtest.py
+++ sources/test_webtest.py
@@ -7,6 +7,6 @@ class test_webtest(unittest.TestCase):
     Test the weather app for functionality
     """
     def test_webl(self):
-        result = ""
         result = go_online()
         self.assertNotEqual(result, "")
+
--- sources/webtest.py
+++ sources/webtest.py
@@ -4,5 +4,4 @@ def go_online():
     url = urllib2.urlopen("https://google.com")
     mybytes = url.read()
     url.close()
-    url = mybytes.decode('utf8')
     return url
