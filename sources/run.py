c770fa7a2c419454c7698aff1cf6c9a05c8b61a5
ian <ianfergu@live.unc.edu>
2019-08-07 09:47:49 -0400

    changed to work with alaska

--- sources/run.py
+++ sources/run.py
@@ -3,4 +3,4 @@ import urllib2
 
 def get_url():
     url = urllib2.urlopen("https://forecast.weather.gov/product.php?site=CRH&product=SFT&issuedby=RAH")
-    print(url)
+    print("The website is up and working! : https://forecast.weather.gov/product.php?site=CRH&product=SFT&issuedby=RAH")
