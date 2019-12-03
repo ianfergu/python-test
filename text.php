afe5c79eddc92684a35ad88dc36ae6110a5c6bbc
ianfergu <42654771+ianfergu@users.noreply.github.com>
2019-12-03 17:55:52 -0500

    Update text.php

--- text.php
+++ text.php
@@ -1,6 +1,6 @@
 <?php 
 echo "<H1>We did it!</H1>";
-$output = shell_exec('ssh fabian@http://comp535-lnx-lampvm1.cs.unc.edu/');
+$output = shell_exec('pwd');
 sleep(10);
 echo "<H1>$output</H1>";
 ?>
